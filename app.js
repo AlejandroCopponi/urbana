758863636fe806437d2b46e7f826afc0dc998def
AlejandroCopponi <82058590+AlejandroCopponi@users.noreply.github.com>
2025-05-31 17:55:52 -0300

    cambiando directorio raiz del service worker

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 // Registrar Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        // La ruta debe ser ABSOLUTA desde la raíz del dominio, incluyendo el subdirectorio
+        // y el scope debe coincidir con el subdirectorio.
+        navigator.serviceWorker.register('/urbana/service-worker.js', { scope: '/urbana/' }) // <-- ¡ASÍ DEBE ESTAR!
             .then(registration => {
                 console.log('Service Worker registrado con éxito:', registration.scope);
             })
